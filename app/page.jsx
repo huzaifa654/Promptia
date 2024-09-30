f020f15c5f478df715b143b2d720e396fd8a1f03
Muhammad Huzaifa <108292736+huzaifa654@users.noreply.github.com>
2024-09-30 17:56:17 +0500

    code

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <section className='w-full flex-center flex-col'>
+            <h1 className='head_text text-center'>Discover & Share </h1>
+        </section>
+    )
+}
+
+export default Home
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,17 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
       },
+      colors: {
+        'primary-orange': '#FF5722',
+      }
     },
   },
   plugins: [],
 }
-export default config
