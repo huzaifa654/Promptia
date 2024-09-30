aebb241d3b499c5a1441f48cb5e0e68b9c9501c4
Muhammad Huzaifa <108292736+huzaifa654@users.noreply.github.com>
2024-09-30 21:24:17 +0500

    code

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Feed from "../components/Feed"
 
 const Home = () => {
     return (
-        <section className='w-full flex-col items-center'>
+        <section className='flex-col text-center'>
             <h1 className='head_text text-center'>Discover & Share
                 <br className='max-md:hidden' />
                 <span className='orange_gradient text-center'>AI-Powered Prompts</span>
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,20 +1,116 @@
 'use client';
+import { get } from 'mongoose';
+import { getProviders, signIn, signOut } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Nav = () => {
+    const IsUserLoggedIn = true;
+    const [providers, setProvider] = useState(null)
+    const [Toggle, setToggle] = useState(false)
+    const setProviders = async () => {
+        const response = await getProviders();
+        setProvider(response)
+    }
+    useEffect(() => {
+        setProviders()
+    }, [])
+
     return (
         <div className='flex-between w-full mb-16 pt-3'>
-            <Link href={'/'}>
+            <Link href={'/'} className='flex gap-2 flex-center'>
                 <Image src={'/assets/images/logo.svg'}
                     alt='Promptia Logo'
                     width={30}
                     height={30}
-                    className='object-contain'
+                    className='object-cont'
                 />
+                <p className='logo_text'>Promptia</p>
             </Link>
-        </div>
+            {/* Desktop Navigation */}
+            <div className='sm:flex hidden'>
+                {IsUserLoggedIn ?
+                    (
+                        <div className='flex gap-3 md:gap-5'>
+                            <Link href={'/craete-prompt'} className='black_btn'>Craete Post</Link>
+                            <button type='button' className='outline_btn' onClick={signOut}>Sign Out</button>
+                            <Link href={'/profile'}>
+                                <Image src={'/assets/images/logo.svg'} width={37} height={37} className='rounded-full' alt='profile' />
+                            </Link>
+                        </div>
+                    )
+                    :
+                    (<>
+                        {
+                            providers && Object.values(providers).map((provider) => {
+                                <button
+                                    type='button'
+                                    key={provider?.name}
+                                    onClick={() => signIn(provider?.id)}
+                                    className='black_btn'
+                                >Sign In
+                                </button>
+                            })
+                        }
+                    </>)
+                }
+            </div>
+
+            {/* Mobile Navigation */}
+            <div className='sm:hidden flex relative'>
+                {IsUserLoggedIn ?
+                    <div className="flex">
+                        <Image
+                            src={'/assets/images/logo.svg'}
+                            width={37}
+                            height={37}
+                            className='rounded-full'
+                            alt='profile'
+                            onClick={() => setToggle(!Toggle)}
+                        />
+                        {Toggle &&
+                            <div className="dropdown">
+                                <Link
+                                    href={'/profile'}
+                                    className='dropdown_link'
+                                    onClick={() => setToggle(false)}
+                                >My Profile</Link>
+                                <Link
+                                    href={'/craete-prompt'}
+                                    className='dropdown_link'
+                                    onClick={() => setToggle(false)}
+                                >Craete Prompt</Link>
+                                <button
+                                    type='button'
+                                    onClick={() => { setToggle(false); signOut() }}
+                                    className='mt-5 w-full black_btn'
+                                >
+                                    Sign Out
+                                </button>
+                            </div>
+                        }
+
+                    </div>
+
+                    :
+                    <>
+                        {
+                            providers && Object.values(providers).map((provider) => {
+                                <button
+                                    type='button'
+                                    key={provider?.name}
+                                    onClick={() => signIn(provider?.id)}
+                                    className='black_btn'
+                                >Sign In
+                                </button>
+                            })
+                        }</>
+
+                }
+
+            </div>
+        </div >
     )
 }
 
