ce9d5fb231e8431528d8108d3adb598eadb50cfb
Muhammad Huzaifa <108292736+huzaifa654@users.noreply.github.com>
2024-10-01 20:45:58 +0500

    code

--- models/users.js
+++ models/users.js
@@ -6,5 +6,14 @@ const UserSchema = newSchema({
         type: String,
         unique: [true, 'Email already exists'],
         required: [true, 'Emai is required!']
+    },
+    username: {
+        type: String,
+        required: [true, "Username is required!"],
+    },
+    image: {
+        type: String
     }
-})
+});
+const User = models.User || model("User", UserSchema);
+export default User;
