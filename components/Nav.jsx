4f460b642c6181a2ab68d20b74208c9e109241fd
Muhammad Huzaifa <108292736+huzaifa654@users.noreply.github.com>
2024-10-01 21:34:35 +0500

    code

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -11,6 +11,7 @@ const Nav = () => {
 
     const IsUserLoggedIn = true;
     const [providers, setProvider] = useState(null)
+    console.log("providers?.google===========", providers?.google)
     const [Toggle, setToggle] = useState(false)
     const setUpProviders = async () => {
         const response = await getProviders();
@@ -33,32 +34,30 @@ const Nav = () => {
             </Link>
             {/* Desktop Navigation */}
             <div className='sm:flex hidden'>
-                {IsUserLoggedIn ?
-                    (
-                        <div className='flex gap-3 md:gap-5'>
-                            <Link href={'/craete-prompt'} className='black_btn'>Craete Post</Link>
-                            <button type='button' className='outline_btn' onClick={signOut}>Sign Out</button>
-                            <Link href={'/profile'}>
-                                <Image src={'/assets/images/logo.svg'} width={37} height={37} className='rounded-full' alt='profile' />
-                            </Link>
-                        </div>
-                    )
-                    :
-                    (<>
-                        {
-                            providers && Object.values(providers).map((provider) => {
-                                <button
-                                    type='button'
-                                    key={provider?.name}
-                                    onClick={() => signIn(provider?.id)}
-                                    className='black_btn'
-                                >Sign In
-                                </button>
-                            })
-                        }
-                    </>)
-                }
+                {session?.user ? (
+                    <div className='flex gap-3 md:gap-5'>
+                        <Link href={'/craete-prompt'} className='black_btn'>Create Post</Link>
+                        <button type='button' className='outline_btn' onClick={signOut}>Sign Out</button>
+                        <Link href={'/profile'}>
+                            <Image src={'/assets/images/logo.svg'} width={37} height={37} className='rounded-full' alt='profile' />
+                        </Link>
+                    </div>
+                ) : (
+                    <>
+                        {providers && Object.values(providers).map((provider) => (
+                            <button
+                                type='button'
+                                key={provider?.name}
+                                onClick={() => signIn(provider?.id)}
+                                className='black_btn'
+                            >
+                                Sign in with {provider?.name}
+                            </button>
+                        ))}
+                    </>
+                )}
             </div>
+
 
             {/* Mobile Navigation */}
             <div className='sm:hidden flex relative'>
@@ -100,13 +99,16 @@ const Nav = () => {
                     <>
                         {
                             providers && Object.values(providers).map((provider) => {
-                                <button
-                                    type='button'
-                                    key={provider?.name}
-                                    onClick={() => signIn(provider?.id)}
-                                    className='black_btn'
-                                >Sign In
-                                </button>
+                                return (
+                                    <button
+                                        type='button'
+                                        key={provider?.name}
+                                        onClick={() => signIn(provider?.id)}
+                                        className='black_btn'
+                                    >Sign In
+                                    </button>
+                                )
+
                             })
                         }</>
 
