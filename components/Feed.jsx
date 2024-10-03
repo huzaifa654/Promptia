8898bf3650a6e541d015ab39df346592fe153bff
Muhammad Huzaifa <108292736+huzaifa654@users.noreply.github.com>
2024-10-03 18:22:10 +0500

    code

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,25 +1,36 @@
 'use client'
 import React, { useState } from 'react'
 
+const PromptCardList=({data,handleTagClick})=>{
+    return(
+        <div className=''>
+
+        </div>
+    )
+}
+
 const Feed = () => {
-    const [SearchText, setSearchText] = useState()
-    const handleSearchText=(e)=>{
+    const [SearchText, setSearchText] = useState('')
+
+    const handleSearchText = (e) => {
         setSearchText(e?.target?.value)
     }
-    return (
-       <section className='feed'>
-        <form  className='relative w-full flex-center'>
-            <input 
-            type="text"
-            placeholder='Search for a tag or a username'
-            value={SearchText}
-            onChange={handleSearchText}
-            required
-            className='search_input_peer'
-            />
-        </form>
 
-       </section>
+
+    return (
+        <section className='mt-16 mx-auto w-full max-w-xl flex justify-center items-center flex-col gap-2'>
+            <form className='relative w-full flex-center'>
+                <input
+                    type="text"
+                    placeholder='Search for a tag or a username'
+                    value={SearchText}
+                    onChange={handleSearchText}
+                    required
+                    className='w-full drop-shadow-md px-3 py-2 rounded'
+                />
+            </form>
+
+        </section>
     )
 }
 
